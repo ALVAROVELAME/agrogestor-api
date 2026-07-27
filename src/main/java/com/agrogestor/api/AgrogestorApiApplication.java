4b7cf236ca86c69b0e09cadb3d2d7e43cf3d1a78
ALVAROVELAME <alvarovelame@gmail.com>
2026-07-27 00:08:13 -0300

    consertar docker

--- src/main/java/com/agrogestor/api/AgrogestorApiApplication.java
+++ src/main/java/com/agrogestor/api/AgrogestorApiApplication.java
@@ -1,4 +1,4 @@
-package com.agrogestor_api;
+package com.agrogestor.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/agrogestor/api/AgrogestorApiApplicationTests.java
+++ src/test/java/com/agrogestor/api/AgrogestorApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.agrogestor_api;
+package com.agrogestor.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
