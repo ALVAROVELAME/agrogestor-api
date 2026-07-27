d38e734b4bbdd71a1b9b40fc501830cfbeac531f
ALVAROVELAME <alvarovelame@gmail.com>
2026-07-26 21:51:01 -0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/agrogestor_api/AgrogestorApiApplication.java
@@ -0,0 +1,13 @@
+package com.agrogestor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrogestorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrogestorApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/agrogestor_api/controller/ApiController.java
@@ -0,0 +1,18 @@
+package com.agrogestor_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class ApiController {
+
+
+    @GetMapping("/")
+    public String status(){
+
+        return "API AgroGestor funcionando";
+
+    }
+
+}
