784add9f9f7f2d536f7457cf85741e6c0e260cf2
ALVAROVELAME <alvarovelame@gmail.com>
2026-07-28 20:38:06 -0300

    atualização de cadastro

--- src/main/java/com/agrogestor/api/service/UsuarioService.java
+++ src/main/java/com/agrogestor/api/service/UsuarioService.java
@@ -4,6 +4,7 @@ package com.agrogestor.api.service;
 import com.agrogestor.api.dto.UsuarioCadastroDTO;
 import com.agrogestor.api.model.CadastroPendente;
 import com.agrogestor.api.repository.CadastroPendenteRepository;
+import com.agrogestor.api.repository.UsuarioRepository;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,8 @@ public class UsuarioService {
 
     private final CadastroPendenteRepository cadastroPendenteRepository;
 
+    private final UsuarioRepository usuarioRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     private final EmailService emailService;
@@ -26,11 +29,13 @@ public class UsuarioService {
 
     public UsuarioService(
             CadastroPendenteRepository cadastroPendenteRepository,
+            UsuarioRepository usuarioRepository,
             PasswordEncoder passwordEncoder,
             EmailService emailService
     ) {
 
         this.cadastroPendenteRepository = cadastroPendenteRepository;
+        this.usuarioRepository = usuarioRepository;
         this.passwordEncoder = passwordEncoder;
         this.emailService = emailService;
 
@@ -39,6 +44,26 @@ public class UsuarioService {
 
 
     public CadastroPendente cadastrar(UsuarioCadastroDTO dto) {
+
+
+        if(usuarioRepository.findByEmail(dto.getEmail()).isPresent()){
+
+            throw new RuntimeException(
+                    "Email já cadastrado"
+            );
+
+        }
+
+
+
+        if(cadastroPendenteRepository.findByEmail(dto.getEmail()).isPresent()){
+
+            throw new RuntimeException(
+                    "Email aguardando confirmação"
+            );
+
+        }
+
 
 
         String senhaHash =
